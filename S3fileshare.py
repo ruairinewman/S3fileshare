6fdee819f7939dcaf7056ca885a974a9558d3806
Ruairi Newman <ruairi.newman@coupa.com>
2021-04-20 12:40:03 +0100

    Region detection made more robust

--- S3fileshare.py
+++ S3fileshare.py
@@ -10,6 +10,8 @@ import magic, os, sys
 from pprint import pprint
 import pyshorteners
 import requests
+import binascii
+import string, random
 
 # Logging configuration
 logname = 's3upload.log'
@@ -17,8 +19,9 @@ logdir = '/logs/'
 logfile = os.environ["HOME"] + logdir + logname
 logging.basicConfig(filename=logfile, encoding='utf-8', level=logging.WARN)
 
+# Argument parsing
 parser = argparse.ArgumentParser(description="Upload file to S3")
-parser.add_argument("-b", "--bucket", help = "S3 bucket name", required=True, type=str)
+parser.add_argument("-b", "--bucket", help = "S3 bucket name", required=False, type=str)
 parser.add_argument("-o", "--object", help = "S3 object name", required=False, type=str)
 parser.add_argument("-f", "--file", help = "File name", required=True, type=str)
 parser.add_argument("-r", "--region", help = "Region", required=False, default=None, type=str)
@@ -26,7 +29,6 @@ parser.add_argument("-e", "--expiry", help = "Expiry in seconds", required=True,
 parser.add_argument("-s", "--short", help = "Require shortened URL", action="store_true")
 args = parser.parse_args()
 
-S3_BUCKET = args.bucket
 FILE = args.file
 if args.object is None:
 	OBJECT = FILE
@@ -35,7 +37,30 @@ else:
 EXPIRY = args.expiry
 
 s3_client = boto3.client('s3')
-region = s3_client.get_bucket_location(Bucket=args.bucket)['LocationConstraint']
+
+if(args.bucket) is not None:
+	region = s3_client.get_bucket_location(Bucket=args.bucket)['LocationConstraint']
+else:
+	try:
+		region = os.environ['AWS_REGION']
+	except:
+		try:
+			region = args.region
+		except:
+			print("Can't identify correct region for bucket. Specify AWS region with '-r' switch.")
+			sys.exit(1)
+
+if args.bucket is None:
+	S3_BUCKET = ''.join(random.choice(string.ascii_lowercase) for i in range(15))
+	try:
+		s3_client.create_bucket(Bucket=S3_BUCKET)
+	except ClientError as e:
+		logging.error(e)
+		pprint(e)
+		sys.exit(1)
+else:
+	S3_BUCKET = args.bucket
+
 s3_client = boto3.client('s3', config=Config(signature_version='s3v4', region_name=region))
 
 def uploadFileS3(FILE, S3_BUCKET, OBJECT, EXPIRY):
